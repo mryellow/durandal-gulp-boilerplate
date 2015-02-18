cb79ecee9571028b5f85ee2775f25ce797a1e2f8
mryellow <mr-yellow@mr-yellow.com>
2015-02-19 10:10:16 +1100

    Add banner to built version

--- build/tasks/durandal.js
+++ build/tasks/durandal.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
+//var gutil = require('gulp-util');
+var fs = require('fs');
 var durandal = require('gulp-durandal');
+var header = require('gulp-header');
 var paths = require('../paths');
 
-gulp.task('durandal', function(){
+gulp.task('durandal', function() {
+	var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+
+	var banner = ['/**',
+		' * Copyright (c) <%= new Date().getFullYear() %> - <%= author %>',
+		' * <%= name %> - <%= description %>',
+		' * @built <%= new Date().toISOString() %>',
+		' * @version v<%= version %>',
+		' * @link <%= homepage %>',
+		' * @license <%= license %>',
+		' */',
+		''].join('\n');
+	
     return durandal({
             baseDir: paths.root,
             main: 'main.js',
@@ -10,10 +25,12 @@ gulp.task('durandal', function(){
             almond: true,
             minify: true,
             rjsConfigAdapter: function(cfg) {
+            	cfg.preserveLicenseComments = true;
             	cfg.generateSourceMaps = false;
 	            cfg.uglify2 = {
+	            	banner: 'test',
 					output: {
-						beautify: true
+						beautify: false
 					},
 					compress: {
 						sequences: false,
@@ -29,5 +46,6 @@ gulp.task('durandal', function(){
             	return cfg;
             }
         })
+    	.pipe(header(banner, pkg))
         .pipe(gulp.dest(paths.output + paths.root));
 });
