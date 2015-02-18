77f4bd43093b8c9a66a4dffeb1f409932c0c03b3
mryellow <mr-yellow@mr-yellow.com>
2015-02-19 09:14:49 +1100

    Extend durandal config

--- build/tasks/durandal.js
+++ build/tasks/durandal.js
@@ -8,7 +8,26 @@ gulp.task('durandal', function(){
             main: 'main.js',
             output: 'main.js',
             almond: true,
-            minify: true
+            minify: true,
+            rjsConfigAdapter: function(cfg) {
+            	cfg.generateSourceMaps = false;
+	            cfg.uglify2 = {
+					output: {
+						beautify: true
+					},
+					compress: {
+						sequences: false,
+						global_defs: {
+							DEBUG: false
+						},
+						drop_console: true
+					},
+					warnings: true,
+					mangle: false
+				};
+
+            	return cfg;
+            }
         })
         .pipe(gulp.dest(paths.output + paths.root));
 });
