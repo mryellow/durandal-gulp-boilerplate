7e1f43e722fc70935058e1c34a8566df427d0642
Mr-Yellow <mr-yellow@mr-yellow.com>
2015-09-07 18:13:42 +1000

    Pinned karma 0.12

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,30 +1,33 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var beep = require('beepbeep');
-var Karma = require('karma').Server;
+var karma = require('karma').server;
+//var Karma = require('karma').server; // 0.13
 
 /**
  * Run test once and exit
  */
 gulp.task('test', function (done) {
-	new Karma({
+	//new Karma({
+	karma.start({
 		configFile: __dirname + '/../../karma.conf.js',
 		singleRun: true
 	}, function(e) {
 		if (e !== 0) beep();
 		done();
 		process.exit(e);
-	}).start();
+	});//.start();
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
 gulp.task('tdd', function (done) {
-	new Karma({
+	//new Karma({
+	karma.start({
 		configFile: __dirname + '/../../karma.conf.js'
 	}, function(e) {
 		if (e !== 0) beep();
 		done();
-	}).start();
+	});//.start();
 });
