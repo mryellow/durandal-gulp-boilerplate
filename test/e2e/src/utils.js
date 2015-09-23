3f71173a608678563a47e4a2d51e06c9d21355e7
Mr-Yellow <mr-yellow@mr-yellow.com>
2015-09-23 11:49:05 +1000

    Refactor e2e promises

--- test/e2e/src/utils.js
+++ test/e2e/src/utils.js
@@ -8,7 +8,7 @@ var Utils = (function () {
     Utils.prototype.isVisible = function (locator) {
         var d = protractor.promise.defer();
         if (!locator) d.cancel();
-                
+
         element(locator).isDisplayed().then(function (displayed) {
             element(locator).getAttribute('style').then(function (style) {
                 d.fulfill((displayed && (style !== 'display: none;') && (style !== 'visibility: hidden;')));
@@ -25,25 +25,18 @@ var Utils = (function () {
 
     // KnockoutJS does not bind to same events as `sendKeys`
     Utils.prototype.koChange = function (id) {
-        var d = protractor.promise.defer();
         // TODO: Better cross-browser support.
         // TODO: Support lookup methods other than `id`.
         var script = "var elem = document.getElementById('"+id+"');" +
             "var event = document.createEvent('HTMLEvents');" +
             "event.initEvent('change', true, true );" +
             "elem.dispatchEvent( event );";
-        browser.executeScript(script).then(function () {
-            d.fulfill();
-        }, function (err) {
-            throw err;
-        });
-        return d.promise;
+        return browser.executeScript(script);
     };
 
     Utils.prototype.koSendKeys = function(locator) {
-        var d = protractor.promise.defer();
         var _self = this;
-        if (!locator || !arguments || !(arguments.length > 1)) d.cancel(); // Warning on length condition, it is clearer this way.
+        if (!locator || !arguments || !(arguments.length > 1)) throw new Error('Invalid arguments passed to koSendKeys');
 
         // Allow for unlimited arguments, to be passed to `sendKeys`.
         var args = [];
@@ -55,46 +48,38 @@ var Utils = (function () {
         // https://github.com/angular/protractor/issues/1734#issuecomment-75791481
         //var elem = browser.driver.findElement(locator);
         //browser.executeScript("arguments[0].scrollIntoView(true);", elem);
-
-        browser.driver.wait(function () {
+        var id;
+        return browser.driver.wait(function () {
             return browser.driver.isElementPresent(locator).then(function (present) {
                 return (present === true);
             });
 
         }).then(function () {
-            browser.driver.wait(function () {
+            return browser.driver.wait(function () {
                 return _self.isVisible(locator);
-
-            }).then(function () {
-                element(locator).getAttribute('id').then(function (id) {
-                    if (id) {
-                        element(locator).clear().then(function () {
-                            element(locator).sendKeys.apply(this, args).then(function () {
-                                _self.koChange(id).then(function () {
-                                    d.fulfill();
-                                });
-                            }, function (err) {
-                                throw err;
-                            });
-                        });
-                    } else {
-                        // `koChange` needs `id` for `getDocumentById`
-                        console.log('An ID attribute must be specified for knockout change event.');
-                        d.cancel();
-                    }
-
-                });
             });
+        }).then(function () {
+            return element(locator).getAttribute('id');
+        }).then(function (val) {
+            // `koChange` needs `id` for `getDocumentById`
+            if (!val) {
+                throw new Error('An ID attribute must be specified for knockout change event.');
+            } else {
+                id = val;
+                return element(locator).clear()
+            }
+        }).then(function () {
+            return element(locator).sendKeys.apply(this, args);
+        }).then(function () {
+            return _self.koChange(id);
         });
-        return d.promise;
     };
 
     Utils.prototype.koSendClick = function(locator) {
-        var d = protractor.promise.defer();
         var _self = this;
-        if (!locator) d.cancel();
+        if (!locator) throw new Error('Invalid locator passed to koSendClick');
 
-        browser.driver.wait(function () {
+        return browser.driver.wait(function () {
             return browser.driver.isElementPresent(locator).then(function (present) {
                 return (present === true);
             });
@@ -106,18 +91,13 @@ var Utils = (function () {
             //var elem = browser.driver.findElement(locator);
             //browser.executeScript("arguments[0].scrollIntoView(true);", elem);
 
-            browser.driver.wait(function () {
+            return browser.driver.wait(function () {
                 return _self.isVisible(locator);
 
-            }).then(function () {
-                element(locator).click().then(function () {
-                    d.fulfill();
-                }, function (err) {
-                    throw err;
-                });
             });
+        }).then(function () {
+            element(locator).click();
         });
-        return d.promise;
     };
 
     return Utils;
